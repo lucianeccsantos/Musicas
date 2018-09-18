9460fe28d1a4e6d8b9c8c1e55ea757e0d37c57c3
Luciane Santos <lucianecarvalho@gmail.com>
2018-09-18 16:04:44 -0300

    Solution

---
+++ Lccs.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lccs.Musicas.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lccs.Musicas.AcessoDados.Entity.Context
+{
+    public class MusicasDbContext: DbContext
+    {
+        public DbSet<Album> Albuns { get; set; }
+    }
+}
